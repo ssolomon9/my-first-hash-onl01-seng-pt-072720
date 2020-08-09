158078c24efcef02e714dbcfbb33738cde4cb23b
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-09 22:29:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  your_hash = {"key" => "value", "another_key" => "another value"}
+  your_hash = {"my_name" => "Solomon", "another_key" => "another value"}
 end
 
 
