44a836eebd14900a92138124e8203651f622f00e
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-09 23:08:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,8 +35,8 @@ shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest[""] = 4
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+puts 
 end
